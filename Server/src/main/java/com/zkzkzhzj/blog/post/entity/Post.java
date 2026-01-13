0faf37b8945c85304b208804263eff807408c071
zkzkzhzj <zkzkzhzj@gmail.com>
2026-01-13 22:34:57 +0900

    feat: Post Entity 뼈대 생성  - 공식문서만 쳐다보려고해서 시간을 너무 잡아먹었다  - 먼저 각 데이터 타입에 맞는 생성 코드를 보고 어노테이션에 관한 정보를 공식문서에서 추가로 찾아보자

--- Server/src/main/java/com/zkzkzhzj/blog/post/entity/Post.java
+++ Server/src/main/java/com/zkzkzhzj/blog/post/entity/Post.java
@@ -1,1 +1,22 @@
-// Entity Class 생성 예정
+package com.zkzkzhzj.blog.post.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+/**
+ * 참고
+ * JPA -> https://docs.spring.io/spring-data/jpa/reference/jpa/getting-started.html
+ * Hibernate ORM -> https://docs.hibernate.org/orm/current/userguide/html_single/#mapping-types
+ */
+
+@Entity
+class Post {
+
+    @Id
+    // Oracle, PostGreSQL 의 경우 시퀀스 오브젝트로 PK 를 증가시킨다. GenerationType.Sequence -> 시퀀스명 연결
+    // MySQL 의 경우 테이블 자체에서 PK 를 관리하기 때문에 데이터가 들어가는 순간 PK 번호 부여(IDENTIFY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
